f4fff1a09705d33bda915e276ac7fdf47c56044a
Vencislav <vencislav67@hotmail.com>
2023-11-26 11:41:59 +0200

    9 task done

---
+++ 9_ToyShop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _9_ToyShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ex = double.Parse(Console.ReadLine());
+            int puz = int.Parse(Console.ReadLine());
+            int doll = int.Parse(Console.ReadLine());
+            int bear = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+            double sum = puz * 2.60 + doll * 3 + bear * 4.10 + min * 8.20 + truck * 2;
+            int br = puz + doll + bear + min + truck;
+            //value = (float)System.Math.Round(value, 2);
+            if (br >= 50)
+            {
+                sum = sum - (sum * 0.25);
+            }
+            sum = sum - (sum * 0.10);
+            if (sum >= ex)
+            {
+                Console.WriteLine($"Yes! {sum - ex:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {ex - sum:f2} lv needed.");
+            }
+        }
+    }
+}
