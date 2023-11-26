83ca155e99275b155e837f2ccac1c14f45ec27d4
Vencislav <vencislav67@hotmail.com>
2023-11-26 11:34:09 +0200

    7th task is done

--- 6_InfoSpeed/Program.cs
+++ 6_InfoSpeed/Program.cs
@@ -6,7 +6,27 @@ namespace _6_InfoSpeed
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = double.Parse(Console.ReadLine());
+            if (a <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (a <= 50 & a > 10)
+            {
+                Console.WriteLine("average");
+            }
+            else if (a > 50 & a <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (a > 150 & a <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
         }
     }
 }
---
+++ 7_AreaFigures/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _7_AreaFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            if (a == "square")
+            {
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine(b * b);
+            }
+            if (a == "rectangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                Console.WriteLine(b * c);
+            }
+            if (a == "circle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine((b * b) * Math.PI);
+            }
+            if (a == "triangle")
+            {
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+                Console.WriteLine((b * c) / 2.0);
+            }
+        }
+    }
+}
