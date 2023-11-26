29d17c3a90765da1e990340a89d096f49795b5ca
Vencislav <vencislav67@hotmail.com>
2023-11-26 11:40:42 +0200

    8th task do ne

---
+++ 8_PlusMin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _8_PlusMin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int hour = a;
+            int min = b + 15;
+            if (min >= 60)
+            {
+                min = min - 60;
+                hour++;
+            }
+            if (hour > 23)
+            {
+                hour = hour - 24;
+            }
+            if (min < 10)
+            {
+                Console.WriteLine($"{hour}:0{min}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{min}");
+            }
+        }
+    }
+}
