68c33767d30f6e4c3f86b34e377fd7d90d359e7e
Vencislav <vencislav67@hotmail.com>
2023-11-26 11:31:19 +0200

    5th task done

---
+++ 5_HundredTo200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_HundredTo200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100 & a <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if (a < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
