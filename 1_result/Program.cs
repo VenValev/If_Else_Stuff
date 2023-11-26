3ce38cd41b96ef154252579e90d834ae8673688a
Vencislav <vencislav67@hotmail.com>
2023-11-26 11:28:18 +0200

    Add project files.

---
+++ 1_result/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
